68c34b12a570a09ca62d73a2748e9f42a6d5b9ba
Mokshit Jain <mokshitjain2006@gmail.com>
2023-01-03 21:23:21 +0530

    fix: change server check to use document

--- packages/react-resizable-panels/src/hooks/useIsomorphicEffect.ts
+++ packages/react-resizable-panels/src/hooks/useIsomorphicEffect.ts
@@ -1,6 +1,6 @@
 import { useEffect, useLayoutEffect } from "react";
 
 const useIsomorphicLayoutEffect =
-  typeof window !== "undefined" ? useLayoutEffect : useEffect;
+  typeof document !== "undefined" ? useLayoutEffect : useEffect;
 
 export default useIsomorphicLayoutEffect;
