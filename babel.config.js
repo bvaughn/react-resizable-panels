ccce2372a29580131659e788162c6f9993edb9e8
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-11-12 15:01:06 -0500

    Transpile of nullish operator to better support older build tools like Webpack 4

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: [require.resolve("@babel/preset-typescript")],
+  plugins: [
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+  ],
 };
