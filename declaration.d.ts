bf49f7f13a2e70fedf14ea0bb23478debab2fd40
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-10 12:48:31 -0500

    Installed stacking-order dependency

--- declaration.d.ts
+++ declaration.d.ts
@@ -2,3 +2,7 @@ declare module "*.module.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "stacking-order" {
+  export function compare(a: Element, b: Element): number;
+}
