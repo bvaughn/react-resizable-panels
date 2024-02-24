c8cb7d6f7c6f2081bbf6fc1a9d8169fe533dcf79
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-24 16:54:07 -0500

    Fix incorrect usage of pageX/pageY coordinates
    This broke resize handle collision detection when scrolling.

--- packages/react-resizable-panels/src/utils/events/getResizeEventCoordinates.ts
+++ packages/react-resizable-panels/src/utils/events/getResizeEventCoordinates.ts
@@ -4,15 +4,15 @@ import { isMouseEvent, isTouchEvent } from ".";
 export function getResizeEventCoordinates(event: ResizeEvent) {
   if (isMouseEvent(event)) {
     return {
-      x: event.pageX,
-      y: event.pageY,
+      x: event.clientX,
+      y: event.clientY,
     };
   } else if (isTouchEvent(event)) {
     const touch = event.touches[0];
-    if (touch && touch.pageX && touch.pageY) {
+    if (touch && touch.clientX && touch.clientY) {
       return {
-        x: touch.pageX,
-        y: touch.pageY,
+        x: touch.clientX,
+        y: touch.clientY,
       };
     }
   }
