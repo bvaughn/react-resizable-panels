9b50f6455e4fffdc4eae07e4e1d9dc1f389bad29
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-05-03 12:05:43 -0400

    Remove test.only

--- packages/react-resizable-panels-website/tests/Storage.spec.ts
+++ packages/react-resizable-panels-website/tests/Storage.spec.ts
@@ -27,7 +27,7 @@ const panelGroupABC = createElement(
 
 test.describe("Storage", () => {
   test.describe("localStorage (default)", () => {
-    test.only("should restore previous layout if autoSaveId prop has been provided", async ({
+    test("should restore previous layout if autoSaveId prop has been provided", async ({
       page,
     }) => {
       await goToUrl(page, panelGroupABC);
