9da8509883c1bfc8faf184f58bbdbf0e4d6e09ce
Niels Widger <niels@qacafe.com>
2024-08-02 12:57:23 -0400

    Fix formatting for vendor/react.ts
    Use formatting required by Prettier for this file.
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>

--- packages/react-resizable-panels/src/vendor/react.ts
+++ packages/react-resizable-panels/src/vendor/react.ts
@@ -37,7 +37,9 @@ const {
 } = React;
 
 // `Math.random()` and `.slice(0, 5)` prevents bundlers from trying to `import { useId } from 'react'`
-const useId = (React as any)[`useId${Math.random()}`.slice(0, 5)] as () => string;
+const useId = (React as any)[
+  `useId${Math.random()}`.slice(0, 5)
+] as () => string;
 
 const useLayoutEffect_do_not_use_directly = useLayoutEffect;
 
