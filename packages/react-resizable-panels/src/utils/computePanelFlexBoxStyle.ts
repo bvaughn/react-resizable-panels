0bfe50b08535970513c1b4d91d23f3d223fb24f0
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-08-23 10:02:39 -0400

    Prettier

--- packages/react-resizable-panels/src/utils/computePanelFlexBoxStyle.ts
+++ packages/react-resizable-panels/src/utils/computePanelFlexBoxStyle.ts
@@ -26,8 +26,7 @@ export function computePanelFlexBoxStyle({
   if (size == null) {
     // Initial render (before panels have registered themselves)
     // In order to support server rendering, fall back to default size if provided
-    flexGrow =
-      defaultSize != undefined ? defaultSize.toFixed(precision) : "1";
+    flexGrow = defaultSize != undefined ? defaultSize.toFixed(precision) : "1";
   } else if (panelData.length === 1) {
     // Special case: Single panel group should always fill full width/height
     flexGrow = "1";
