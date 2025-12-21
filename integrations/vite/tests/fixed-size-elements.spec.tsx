9b4c9b9173e53694ea9241cba0dbfa726eb92fc8
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-12-21 10:32:30 -0500

    Run more e2e tests in a popup window

--- integrations/vite/tests/fixed-size-elements.spec.tsx
+++ integrations/vite/tests/fixed-size-elements.spec.tsx
@@ -3,144 +3,160 @@ import { Group, Panel, Separator } from "react-resizable-panels";
 import { goToUrl } from "./utils/goToUrl";
 
 test.describe("fixed size elements", () => {
-  test("should not be interactive directly", async ({ page }) => {
-    await goToUrl(
-      page,
-      <Group>
-        <Panel id="foo" />
-        <div>foo+bar</div>
-        <Panel id="bar" />
-        <Separator id="bar+baz" />
-        <div>bar+baz</div>
-        <Panel id="baz" />
-        <Separator id="baz+qux+left" />
-        <div>baz+qux</div>
-        <Separator id="baz+qux+right" />
-        <Panel id="qux" />
-      </Group>
-    );
+  for (const usePopUpWindow of [true, false]) {
+    test.describe(usePopUpWindow ? "in a popup" : "in the main window", () => {
+      test("should not be interactive directly", async ({ page: mainPage }) => {
+        const page = await goToUrl(
+          mainPage,
+          <Group>
+            <Panel id="foo" />
+            <div>foo+bar</div>
+            <Panel id="bar" />
+            <Separator id="bar+baz" />
+            <div>bar+baz</div>
+            <Panel id="baz" />
+            <Separator id="baz+qux+left" />
+            <div>baz+qux</div>
+            <Separator id="baz+qux+right" />
+            <Panel id="qux" />
+          </Group>,
+          { usePopUpWindow }
+        );
 
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
 
-    for (const text of ["foo+bar", "bar+baz", "baz+qux"]) {
-      const box = (await page.getByText(text).boundingBox())!;
+        for (const text of ["foo+bar", "bar+baz", "baz+qux"]) {
+          const box = (await page.getByText(text).boundingBox())!;
 
-      await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
-      await page.mouse.down();
-      await page.mouse.move(0, 0);
-      await page.mouse.up();
-    }
+          await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+          await page.mouse.down();
+          await page.mouse.move(0, 0);
+          await page.mouse.up();
+        }
 
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
-  });
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
+      });
 
-  test("should work without an explicit separator", async ({ page }) => {
-    await goToUrl(
-      page,
-      <Group>
-        <Panel id="foo" />
-        <div>foo+bar</div>
-        <Panel id="bar" />
-        <Separator id="bar+baz" />
-        <div>bar+baz</div>
-        <Panel id="baz" />
-        <Separator id="baz+qux+left" />
-        <div>baz+qux</div>
-        <Separator id="baz+qux+right" />
-        <Panel id="qux" />
-      </Group>
-    );
+      test("should work without an explicit separator", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <Group>
+            <Panel id="foo" />
+            <div>foo+bar</div>
+            <Panel id="bar" />
+            <Separator id="bar+baz" />
+            <div>bar+baz</div>
+            <Panel id="baz" />
+            <Separator id="baz+qux+left" />
+            <div>baz+qux</div>
+            <Separator id="baz+qux+right" />
+            <Panel id="qux" />
+          </Group>,
+          { usePopUpWindow }
+        );
 
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
 
-    const boxFoo = (await page.getByText("id: foo").boundingBox())!;
+        const boxFoo = (await page.getByText("id: foo").boundingBox())!;
 
-    await page.mouse.move(boxFoo.x + boxFoo.width, boxFoo.y);
-    await page.mouse.down();
-    await page.mouse.move(0, 0);
-    await page.mouse.up();
+        await page.mouse.move(boxFoo.x + boxFoo.width, boxFoo.y);
+        await page.mouse.down();
+        await page.mouse.move(0, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 2')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 2')).toBeVisible();
 
-    const boxBar = (await page.getByText("id: bar").boundingBox())!;
+        const boxBar = (await page.getByText("id: bar").boundingBox())!;
 
-    await page.mouse.move(boxBar.x, boxBar.y);
-    await page.mouse.down();
-    await page.mouse.move(1000, 0);
-    await page.mouse.up();
+        await page.mouse.move(boxBar.x, boxBar.y);
+        await page.mouse.down();
+        await page.mouse.move(1000, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 3')).toBeVisible();
-  });
+        await expect(mainPage.getByText('"onLayoutCount": 3')).toBeVisible();
+      });
 
-  test("should work with an explicit separator", async ({ page }) => {
-    await goToUrl(
-      page,
-      <Group>
-        <Panel id="foo" />
-        <div>foo+bar</div>
-        <Panel id="bar" />
-        <Separator id="bar+baz" />
-        <div>bar+baz</div>
-        <Panel id="baz" />
-        <Separator id="baz+qux+left" />
-        <div>baz+qux</div>
-        <Separator id="baz+qux+right" />
-        <Panel id="qux" />
-      </Group>
-    );
+      test("should work with an explicit separator", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <Group>
+            <Panel id="foo" />
+            <div>foo+bar</div>
+            <Panel id="bar" />
+            <Separator id="bar+baz" />
+            <div>bar+baz</div>
+            <Panel id="baz" />
+            <Separator id="baz+qux+left" />
+            <div>baz+qux</div>
+            <Separator id="baz+qux+right" />
+            <Panel id="qux" />
+          </Group>,
+          { usePopUpWindow }
+        );
 
-    const separatorBox = (await page.getByTestId("bar+baz").boundingBox())!;
+        const separatorBox = (await page.getByTestId("bar+baz").boundingBox())!;
 
-    await page.mouse.move(separatorBox.x, separatorBox.y);
-    await page.mouse.down();
-    await page.mouse.move(0, 0);
-    await page.mouse.up();
+        await page.mouse.move(separatorBox.x, separatorBox.y);
+        await page.mouse.down();
+        await page.mouse.move(0, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 2')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 2')).toBeVisible();
 
-    const boxBaz = (await page.getByText("id: baz").boundingBox())!;
+        const boxBaz = (await page.getByText("id: baz").boundingBox())!;
 
-    await page.mouse.move(boxBaz.x, boxBaz.y);
-    await page.mouse.down();
-    await page.mouse.move(1000, 0);
-    await page.mouse.up();
+        await page.mouse.move(boxBaz.x, boxBaz.y);
+        await page.mouse.down();
+        await page.mouse.move(1000, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 3')).toBeVisible();
-  });
+        await expect(mainPage.getByText('"onLayoutCount": 3')).toBeVisible();
+      });
 
-  test("should work with two explicit separators", async ({ page }) => {
-    await goToUrl(
-      page,
-      <Group>
-        <Panel id="foo" />
-        <div>foo+bar</div>
-        <Panel id="bar" />
-        <Separator id="bar+baz" />
-        <div>bar+baz</div>
-        <Panel id="baz" />
-        <Separator id="baz+qux+left" />
-        <div>baz+qux</div>
-        <Separator id="baz+qux+right" />
-        <Panel id="qux" />
-      </Group>
-    );
+      test("should work with two explicit separators", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <Group>
+            <Panel id="foo" />
+            <div>foo+bar</div>
+            <Panel id="bar" />
+            <Separator id="bar+baz" />
+            <div>bar+baz</div>
+            <Panel id="baz" />
+            <Separator id="baz+qux+left" />
+            <div>baz+qux</div>
+            <Separator id="baz+qux+right" />
+            <Panel id="qux" />
+          </Group>,
+          { usePopUpWindow }
+        );
 
-    let separatorBox = (await page.getByTestId("baz+qux+left").boundingBox())!;
+        let separatorBox = (await page
+          .getByTestId("baz+qux+left")
+          .boundingBox())!;
 
-    await page.mouse.move(separatorBox.x, separatorBox.y);
-    await page.mouse.down();
-    await page.mouse.move(0, 0);
-    await page.mouse.up();
+        await page.mouse.move(separatorBox.x, separatorBox.y);
+        await page.mouse.down();
+        await page.mouse.move(0, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 2')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 2')).toBeVisible();
 
-    separatorBox = (await page.getByTestId("baz+qux+right").boundingBox())!;
+        separatorBox = (await page.getByTestId("baz+qux+right").boundingBox())!;
 
-    await page.mouse.move(separatorBox.x, separatorBox.y);
-    await page.mouse.down();
-    await page.mouse.move(1000, 0);
-    await page.mouse.up();
+        await page.mouse.move(separatorBox.x, separatorBox.y);
+        await page.mouse.down();
+        await page.mouse.move(1000, 0);
+        await page.mouse.up();
 
-    await expect(page.getByText('"onLayoutCount": 3')).toBeVisible();
-  });
+        await expect(mainPage.getByText('"onLayoutCount": 3')).toBeVisible();
+      });
+    });
+  }
 });
--- integrations/vite/tests/visibility.spec.tsx
+++ integrations/vite/tests/visibility.spec.tsx
@@ -4,82 +4,88 @@ import { DisplayModeToggle } from "../src/components/DisplayModeToggle";
 import { goToUrl } from "./utils/goToUrl";
 
 test.describe("visibility", () => {
-  test("Activity API should defer default layout calculation until visible", async ({
-    page
-  }) => {
-    await goToUrl(
-      page,
-      <DisplayModeToggle mode="activity">
-        <Group>
-          <Panel defaultSize="25%" id="left" minSize={150} />
-          <Separator />
-          <Panel id="right" />
-        </Group>
-      </DisplayModeToggle>
-    );
+  for (const usePopUpWindow of [true, false]) {
+    test.describe(usePopUpWindow ? "in a popup" : "in the main window", () => {
+      test("Activity API should defer default layout calculation until visible", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <DisplayModeToggle mode="activity">
+            <Group>
+              <Panel defaultSize="25%" id="left" minSize={150} />
+              <Separator />
+              <Panel id="right" />
+            </Group>
+          </DisplayModeToggle>,
+          { usePopUpWindow }
+        );
 
-    await expect(page.getByText('"onLayoutCount": 0')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 0')).toBeVisible();
 
-    await page.getByText("hidden → visible").click();
+        await page.getByText("hidden → visible").click();
 
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
-    await expect(page.getByText("id: left")).toContainText("25%");
-    await expect(page.getByRole("separator")).toBeVisible();
-    await expect(page.getByText("id: right")).toContainText("75%");
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
+        await expect(page.getByText("id: left")).toContainText("25%");
+        await expect(page.getByRole("separator")).toBeVisible();
+        await expect(page.getByText("id: right")).toContainText("75%");
 
-    await page.getByText("visible → hidden").click();
+        await page.getByText("visible → hidden").click();
 
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
 
-    await page.setViewportSize({
-      width: 500,
-      height: 500
+        await page.setViewportSize({
+          width: 500,
+          height: 500
+        });
+        await page.getByText("hidden → visible").click();
+
+        await expect(mainPage.getByText('"onLayoutCount": 2')).toBeVisible();
+        await expect(page.getByText("id: left")).toContainText("32%");
+        await expect(page.getByRole("separator")).toBeVisible();
+        await expect(page.getByText("id: right")).toContainText("68%");
+      });
+
+      test("display:hidden should defer default layout calculation until visible", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <DisplayModeToggle mode="css">
+            <Group>
+              <Panel defaultSize="25%" id="left" minSize={150} />
+              <Separator />
+              <Panel id="right" />
+            </Group>
+          </DisplayModeToggle>,
+          { usePopUpWindow }
+        );
+
+        await expect(mainPage.getByText('"onLayoutCount": 0')).toBeVisible();
+
+        await page.getByText("hidden → visible").click();
+
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
+        await expect(page.getByText("id: left")).toContainText("25%");
+        await expect(page.getByRole("separator")).toBeVisible();
+        await expect(page.getByText("id: right")).toContainText("75%");
+
+        await page.getByText("visible → hidden").click();
+
+        await expect(mainPage.getByText('"onLayoutCount": 1')).toBeVisible();
+        // Actual layout values are not meaningful since the Group is not visible
+
+        await page.setViewportSize({
+          width: 500,
+          height: 500
+        });
+        await page.getByText("hidden → visible").click();
+
+        await expect(mainPage.getByText('"onLayoutCount": 2')).toBeVisible();
+        await expect(page.getByText("id: left")).toContainText("32%");
+        await expect(page.getByRole("separator")).toBeVisible();
+        await expect(page.getByText("id: right")).toContainText("68%");
+      });
     });
-    await page.getByText("hidden → visible").click();
-
-    await expect(page.getByText('"onLayoutCount": 2')).toBeVisible();
-    await expect(page.getByText("id: left")).toContainText("32%");
-    await expect(page.getByRole("separator")).toBeVisible();
-    await expect(page.getByText("id: right")).toContainText("68%");
-  });
-
-  test("display:hidden should defer default layout calculation until visible", async ({
-    page
-  }) => {
-    await goToUrl(
-      page,
-      <DisplayModeToggle mode="css">
-        <Group>
-          <Panel defaultSize="25%" id="left" minSize={150} />
-          <Separator />
-          <Panel id="right" />
-        </Group>
-      </DisplayModeToggle>
-    );
-
-    await expect(page.getByText('"onLayoutCount": 0')).toBeVisible();
-
-    await page.getByText("hidden → visible").click();
-
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
-    await expect(page.getByText("id: left")).toContainText("25%");
-    await expect(page.getByRole("separator")).toBeVisible();
-    await expect(page.getByText("id: right")).toContainText("75%");
-
-    await page.getByText("visible → hidden").click();
-
-    await expect(page.getByText('"onLayoutCount": 1')).toBeVisible();
-    // Actual layout values are not meaningful since the Group is not visible
-
-    await page.setViewportSize({
-      width: 500,
-      height: 500
-    });
-    await page.getByText("hidden → visible").click();
-
-    await expect(page.getByText('"onLayoutCount": 2')).toBeVisible();
-    await expect(page.getByText("id: left")).toContainText("32%");
-    await expect(page.getByRole("separator")).toBeVisible();
-    await expect(page.getByText("id: right")).toContainText("68%");
-  });
+  }
 });
