8c80574d2f378bd12b401df2c205593bf5baa576
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-01-04 07:45:26 -0500

    Added a note to Common Questions page about pixels vs percentages

--- src/routes/CommonQuestionsRoute.tsx
+++ src/routes/CommonQuestionsRoute.tsx
@@ -1,4 +1,4 @@
-import { Box, Code, Header } from "react-lib-tools";
+import { Box, Callout, Code, Header, Link } from "react-lib-tools";
 import { html as VerticalHTML } from "../../public/generated/examples/LayoutBasicsVertical.json";
 import { html as VerticalGroupOverflowHTML } from "../../public/generated/examples/VerticalGroupOverflow.json";
 
@@ -7,7 +7,35 @@ export default function CommonQuestionsRoute() {
     <Box direction="column" gap={4}>
       <Header title="Common questions" />
       <dl>
-        <dt className="text-lg text-fuchsia-300 font-bold">
+        <dt
+          className="text-lg text-fuchsia-300 font-bold"
+          id="invalid-panel-layout"
+        >
+          Why do I see an "invalid panel layout" error?
+        </dt>
+        <dd className="mt-2 mb-4 flex flex-col gap-2">
+          <p>
+            This error means that a <code>Group</code> layout (or the sum total
+            of <code>Panel</code> default sizes) does not add up to 100%.
+          </p>
+          <p>
+            If the layout you've specified does add up to 100, the most likely
+            remaining cause is that you've specified sizes as <em>pixels</em>{" "}
+            rather than <em>percentages</em> as per this note from the{" "}
+            <Link to="/props/panel">
+              <code>Panel</code> docs
+            </Link>
+            :
+          </p>
+          <Callout intent="primary" minimal>
+            Numeric values are assumed to be pixels. Strings without explicit
+            units are assumed to be percentages (0%..100%).
+          </Callout>
+        </dd>
+        <dt
+          className="text-lg text-fuchsia-300 font-bold"
+          id="vertical-group-height"
+        >
           Why is a vertical group not visible?
         </dt>
         <dd className="mt-2 mb-4 flex flex-col gap-2">
@@ -21,7 +49,10 @@ export default function CommonQuestionsRoute() {
           </p>
           <Code html={VerticalHTML} />
         </dd>
-        <dt className="text-lg text-fuchsia-300 font-bold">
+        <dt
+          className="text-lg text-fuchsia-300 font-bold"
+          id="horizontal-group-height"
+        >
           Why is a horizontal group's content too tall?
         </dt>
         <dd className="mt-2 mb-4 flex flex-col gap-2">
