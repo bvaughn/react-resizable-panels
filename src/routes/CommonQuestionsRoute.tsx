0a89acf2d4a58ee54768a3341a5d9ea48b74bedf
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-01-04 07:52:16 -0500

    Improved anchor tag top margin

--- src/routes/CommonQuestionsRoute.tsx
+++ src/routes/CommonQuestionsRoute.tsx
@@ -1,19 +1,17 @@
 import { Box, Callout, Code, Header, Link } from "react-lib-tools";
 import { html as VerticalHTML } from "../../public/generated/examples/LayoutBasicsVertical.json";
 import { html as VerticalGroupOverflowHTML } from "../../public/generated/examples/VerticalGroupOverflow.json";
+import type { PropsWithChildren } from "react";
 
 export default function CommonQuestionsRoute() {
   return (
     <Box direction="column" gap={4}>
       <Header title="Common questions" />
       <dl>
-        <dt
-          className="text-lg text-fuchsia-300 font-bold"
-          id="invalid-panel-layout"
-        >
+        <Question id="invalid-panel-layout">
           Why do I see an "invalid panel layout" error?
-        </dt>
-        <dd className="mt-2 mb-4 flex flex-col gap-2">
+        </Question>
+        <Answer>
           <p>
             This error means that a <code>Group</code> layout (or the sum total
             of <code>Panel</code> default sizes) does not add up to 100%.
@@ -31,14 +29,11 @@ export default function CommonQuestionsRoute() {
             Numeric values are assumed to be pixels. Strings without explicit
             units are assumed to be percentages (0%..100%).
           </Callout>
-        </dd>
-        <dt
-          className="text-lg text-fuchsia-300 font-bold"
-          id="vertical-group-height"
-        >
+        </Answer>
+        <Question id="vertical-group-height">
           Why is a vertical group not visible?
-        </dt>
-        <dd className="mt-2 mb-4 flex flex-col gap-2">
+        </Question>
+        <Answer>
           <p>
             <code>Group</code> is a block-level element and so it will fill the
             width of the container it is rendered within. Horizontal groups will
@@ -48,14 +43,11 @@ export default function CommonQuestionsRoute() {
             props).
           </p>
           <Code html={VerticalHTML} />
-        </dd>
-        <dt
-          className="text-lg text-fuchsia-300 font-bold"
-          id="horizontal-group-height"
-        >
+        </Answer>
+        <Question id="horizontal-group-height">
           Why is a horizontal group's content too tall?
-        </dt>
-        <dd className="mt-2 mb-4 flex flex-col gap-2">
+        </Question>
+        <Answer>
           <p>
             Horizontal <code>Groups</code> will fill expand to fit the height of
             their <code>Panel</code> contents. In this behavior is unwanted
@@ -63,8 +55,22 @@ export default function CommonQuestionsRoute() {
             an explicit height to the <code>Groups</code> element.
           </p>
           <Code html={VerticalGroupOverflowHTML} />
-        </dd>
+        </Answer>
       </dl>
     </Box>
   );
+}
+
+function Question({ children, id }: PropsWithChildren<{ id: string }>) {
+  return (
+    <dt className="pt-4" id={id}>
+      <a className="text-lg text-fuchsia-300! font-bold" href={`#${id}`}>
+        {children}
+      </a>
+    </dt>
+  );
+}
+
+function Answer({ children }: PropsWithChildren) {
+  return <dd className="mt-2 flex flex-col gap-2">{children}</dd>;
 }
