31d0bce0273e29b66954a5cb7697870d5ca7a19a
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-01-13 18:24:46 -0500

    bugfix: Support defaultSize=0

--- integrations/tests/tests/default-sizes.spec.tsx
+++ integrations/tests/tests/default-sizes.spec.tsx
@@ -12,14 +12,17 @@ test.describe("default panel sizes", () => {
           <Group>
             <Panel defaultSize="30%" id="left" minSize={50} />
             <Separator />
+            <Panel collapsible defaultSize="0%" id="middle" minSize={50} />
+            <Separator />
             <Panel id="right" minSize={50} />
           </Group>,
           { usePopUpWindow }
         );
 
         await expect(page.getByText("id: left")).toContainText("30%");
-        await expect(page.getByRole("separator")).toBeVisible();
+        await expect(page.getByText("id: middle")).toContainText("0%");
         await expect(page.getByText("id: right")).toContainText("70%");
+        await expect(page.getByRole("separator")).toHaveCount(2);
       });
 
       test("pixels", async ({ page: mainPage }) => {
@@ -28,14 +31,17 @@ test.describe("default panel sizes", () => {
           <Group>
             <Panel defaultSize={200} id="left" minSize={50} />
             <Separator />
+            <Panel collapsible defaultSize={0} id="middle" minSize={50} />
+            <Separator />
             <Panel id="right" minSize={50} />
           </Group>,
           { usePopUpWindow }
         );
 
         await expect(page.getByText("id: left")).toContainText("200px");
-        await expect(page.getByRole("separator")).toBeVisible();
-        await expect(page.getByText("id: right")).toContainText("776px");
+        await expect(page.getByText("id: middle")).toContainText("0px");
+        await expect(page.getByText("id: right")).toContainText("752px");
+        await expect(page.getByRole("separator")).toHaveCount(2);
       });
 
       test("rems", async ({ page: mainPage }) => {
@@ -44,14 +50,17 @@ test.describe("default panel sizes", () => {
           <Group>
             <Panel defaultSize="10rem" id="left" minSize={50} />
             <Separator />
+            <Panel collapsible defaultSize="0rem" id="middle" minSize={50} />
+            <Separator />
             <Panel id="right" minSize={50} />
           </Group>,
           { usePopUpWindow }
         );
 
         await expect(page.getByText("id: left")).toContainText("160px");
-        await expect(page.getByRole("separator")).toBeVisible();
-        await expect(page.getByText("id: right")).toContainText("816px");
+        await expect(page.getByText("id: middle")).toContainText("0px");
+        await expect(page.getByText("id: right")).toContainText("792px");
+        await expect(page.getByRole("separator")).toHaveCount(2);
       });
 
       test("vw", async ({ page: mainPage }) => {
@@ -60,14 +69,17 @@ test.describe("default panel sizes", () => {
           <Group>
             <Panel defaultSize="25vw" id="left" minSize={50} />
             <Separator />
+            <Panel collapsible defaultSize="0vw" id="middle" minSize={50} />
+            <Separator />
             <Panel id="right" minSize={50} />
           </Group>,
           { usePopUpWindow }
         );
 
         await expect(page.getByText("id: left")).toContainText("250px");
-        await expect(page.getByRole("separator")).toBeVisible();
-        await expect(page.getByText("id: right")).toContainText("726px");
+        await expect(page.getByText("id: middle")).toContainText("0px");
+        await expect(page.getByText("id: right")).toContainText("702px");
+        await expect(page.getByRole("separator")).toHaveCount(2);
       });
     });
   }
