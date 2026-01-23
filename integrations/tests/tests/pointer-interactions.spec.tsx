6280ea67e14490e31d63b0ec5d2f6c4ca1da7cc5
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-01-23 17:22:11 -0500

    Double-clicking a Separator resets Panel to default size

--- integrations/tests/tests/pointer-interactions.spec.tsx
+++ integrations/tests/tests/pointer-interactions.spec.tsx
@@ -366,6 +366,34 @@ test.describe("pointer interactions", () => {
         await expect(mainPage.getByText('"right": 33')).toBeVisible();
       });
 
+      test("double-clicking a separator resets panel to default size", async ({
+        page: mainPage
+      }) => {
+        const page = await goToUrl(
+          mainPage,
+          <Group>
+            <Panel defaultSize="30%" id="left" minSize={50} />
+            <Separator id="separator" />
+            <Panel id="right" minSize={50} />
+          </Group>,
+          { usePopUpWindow }
+        );
+
+        await assertLayoutChangeCounts(mainPage, 1);
+        await expect(mainPage.getByText('"left": 30')).toBeVisible();
+
+        await resizeHelper(page, ["left", "right"], 100, 0);
+
+        await assertLayoutChangeCounts(mainPage, 2);
+        await expect(mainPage.getByText('"left": 40')).toBeVisible();
+
+        const separator = page.getByTestId("separator");
+        await separator.dblclick();
+
+        await assertLayoutChangeCounts(mainPage, 3);
+        await expect(mainPage.getByText('"left": 30')).toBeVisible();
+      });
+
       test.describe("focus", () => {
         test("should update focus to the nearest separator", async ({
           page: mainPage
---
+++ lib/global/event-handlers/onDocumentDoubleClick.ts
@@ -0,0 +1,42 @@
+import type { RegisteredGroup } from "../../components/group/types";
+import type { RegisteredPanel } from "../../components/panel/types";
+import { read } from "../mutableState";
+import { findMatchingHitRegions } from "../utils/findMatchingHitRegions";
+import { getImperativePanelMethods } from "../utils/getImperativePanelMethods";
+
+export function onDocumentDoubleClick(event: MouseEvent) {
+  if (event.defaultPrevented) {
+    return;
+  }
+
+  const { mountedGroups } = read();
+
+  const hitRegions = findMatchingHitRegions(event, mountedGroups);
+
+  const groups = new Set<RegisteredGroup>();
+  const panels = new Set<RegisteredPanel>();
+
+  hitRegions.forEach((current) => {
+    groups.add(current.group);
+    current.panels.forEach((panel) => {
+      panels.add(panel);
+    });
+
+    if (current.separator) {
+      const primaryPanel = current.panels[0];
+      if (primaryPanel.panelConstraints.defaultSize !== undefined) {
+        const api = getImperativePanelMethods({
+          groupId: current.group.id,
+          panelId: primaryPanel.id
+        });
+        if (api) {
+          api.resize(primaryPanel.panelConstraints.defaultSize);
+        }
+      }
+    }
+  });
+
+  if (hitRegions.length) {
+    event.preventDefault();
+  }
+}
