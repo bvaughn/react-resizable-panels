bvaughn/react-resizable-panels

commit 1b5bef9ddef56f19712dcd82a88a321049153495

4.3.0 (#583)
The primary change in this version is to the `Group` component, which
now sets default `width`, `height`, and `overflow` styles. (Both can be
overridden using the `style` property.) This is being done to improve
DevX and reduce confusion with groups either being invisible or too
tall.

This mirrors the behavior of default group styles in versions 2 and 3.:

https://github.com/bvaughn/react-resizable-panels/blob/43a071df6bb96d54e8b81a6be0e2d453af45cc4b/packages/react-resizable-panels/src/PanelGroup.ts#L933-L939

> [!NOTE]
> Is this a breaking change or a bug fix? I see arguments for both
sides. I lean towards thinking of it as a bug fix though, after having
migrated a lot of apps from version 3 to 4 today. I think this behavior
is a more intuitive _default_ that can still be overridden in several
ways.

This version also includes the following minor changes:
- Drag interactions only call `event.preventDefault` for the primary
button
- Refine TS types for `useGroupRef` and `usePanelRef` to include `|
null` to increase compatibility with older React versions
- Update TSDoc comments for `Panel` and `Separator` components

lib/components/panel/Panel.tsx
@@ -34,6 +34,8 @@ import { usePanelImperativeHandle } from "./usePanelImperativeHandle";
  * ```
  *
  * ℹ️ [Test id](https://testing-library.com/docs/queries/bytestid/) can be used to narrow selection when unit testing.
+ *
+ * ⚠️ Panel elements must be direct DOM children of their parent Group elements.
  */
 export function Panel({
   children,

integrations/vite/tests/cursor.spec.tsx
@@ -90,7 +90,7 @@ test.describe("cursor", () => {
       test("intersecting", async ({ page: mainPage }) => {
         const page = await goToUrl(
           mainPage,
-          <Group orientation="vertical">
+          <Group className="h-[250px]!" orientation="vertical">
             <Panel id="top" minSize="25%" />
             <Separator id="vertical-separator" />
             <Panel id="bottom" minSize="25%">

lib/components/group/Group.tsx
@@ -258,6 +258,9 @@ export function Group({
         id={id}
         ref={mergedRef}
         style={{
+          height: "100%",
+          width: "100%",
+          overflow: "hidden",
           ...style,
           ...cssVariables,
           display: "flex",

lib/components/group/types.ts
@@ -104,7 +104,7 @@ export type GroupProps = HTMLAttributes<HTMLDivElement> & {
   /**
    * Ref attached to the root `HTMLDivElement`.
    */
-  elementRef?: Ref<HTMLDivElement> | undefined;
+  elementRef?: Ref<HTMLDivElement | null> | undefined;
 
   /**
    * Exposes the following imperative API:
@@ -113,7 +113,7 @@ export type GroupProps = HTMLAttributes<HTMLDivElement> & {
    *
    * ℹ️ The `useGroupRef` and `useGroupCallbackRef` hooks are exported for convenience use in TypeScript projects.
    */
-  groupRef?: Ref<GroupImperativeHandle> | undefined;
+  groupRef?: Ref<GroupImperativeHandle | null> | undefined;
 
   /**
    * Uniquely identifies this group within an application.

lib/components/group/useGroupRef.ts
@@ -5,5 +5,5 @@ import type { GroupImperativeHandle } from "./types";
  * Convenience hook to return a properly typed ref for the Group component.
  */
 export function useGroupRef() {
-  return useRef<GroupImperativeHandle>(null);
+  return useRef<GroupImperativeHandle | null>(null);
 }

lib/components/panel/types.ts
@@ -118,7 +118,7 @@ export type PanelProps = BasePanelAttributes & {
   /**
    * Ref attached to the root `HTMLDivElement`.
    */
-  elementRef?: Ref<HTMLDivElement> | undefined;
+  elementRef?: Ref<HTMLDivElement | null> | undefined;
 
   /**
    * Uniquely identifies this panel within the parent group.
@@ -165,7 +165,7 @@ export type PanelProps = BasePanelAttributes & {
    *
    * ℹ️ The `usePanelRef` and `usePanelCallbackRef` hooks are exported for convenience use in TypeScript projects.
    */
-  panelRef?: Ref<PanelImperativeHandle> | undefined;
+  panelRef?: Ref<PanelImperativeHandle | null> | undefined;
 
   /**
    * CSS properties.

lib/components/panel/usePanelRef.ts
@@ -5,5 +5,5 @@ import type { PanelImperativeHandle } from "./types";
  * Convenience hook to return a properly typed ref for the Panel component.
  */
 export function usePanelRef() {
-  return useRef<PanelImperativeHandle>(null);
+  return useRef<PanelImperativeHandle | null>(null);
 }

lib/components/separator/Separator.tsx
@@ -13,7 +13,7 @@ import type { RegisteredSeparator, SeparatorProps } from "./types";
 /**
  * Separators are not _required_ but they are _recommended_ as they improve keyboard accessibility.
  *
- * Separators should be rendered as the direct child of a Group component.
+ * ⚠️ Separator elements must be direct DOM children of their parent Group elements.
  *
  * Separator elements always include the following attributes:
  *

commit 3dd3b17b707e826bb027ad99374604e813020639

Bugfix: Panel heights within vertical groups (#622)
This regressed in PR #583 with the introduction of `height: 100%`. If
`min-height` was used to set the Group's height (rather than an
`!important` height override) then Panels would not fully fill the
available space in Safari.

This [Stack Overflow
comment](https://stackoverflow.com/questions/8468066/child-inside-parent-with-min-height-100-not-inheriting-height)
explains the reason why:

> An element grows in height as much as required by its inner content.
In such a situation, if the child also says I should have 100% height of
my parent (through height:100%), it becomes a deadlock - parent height
dependent on child, child height dependent on parent's. This deadlock
cannot be resolved by the browser and so height:100% simply doesn't
works...unless the parent as a resolvable height - one way to do which
is by giving an explicit height to parent

This PR fixes that by essentially switching the inner Panel element to
use `flex-grow: 1` rather than `height: 100%`

Resolves #619

lib/components/panel/Panel.tsx
@@ -124,6 +124,7 @@ export function Panel({
       style={{
         ...PROHIBITED_CSS_PROPERTIES,
 
+        display: "flex",
         flexBasis: 0,
         flexShrink: 1,
 
@@ -136,8 +137,8 @@ export function Panel({
       <div
         className={className}
         style={{
-          width: "100%",
-          height: "100%",
+          flexGrow: 1,
+
           ...style
         }}
       >