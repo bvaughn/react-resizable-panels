e8dd4300a1db74b127d91e77fc81b16ed0bd4af8
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-03-14 11:38:03 -0400

    Panel imperative API resize() accepts all size units (#690)
    Resolves #687

--- lib/utils/test/mockGetComputedStyle.ts
+++ lib/utils/test/mockGetComputedStyle.ts
@@ -2,24 +2,29 @@ import { EventEmitter } from "stream";
 
 const elementToStyle = new Map<Element, CSSStyleDeclaration>();
 
+const originalGetComputedStyle = window.getComputedStyle;
+
 let defaultStyle: CSSStyleDeclaration | undefined = undefined;
+
+const emptyStyle = {} as CSSStyleDeclaration;
 
 export const emitter = new EventEmitter();
 emitter.setMaxListeners(100);
 
 export function mockGetComputedStyle() {
-  const originalGetComputedStyle = window.getComputedStyle;
-
   window.getComputedStyle = function getComputedStyle(element: Element) {
-    return (
-      elementToStyle.get(element) ??
-      defaultStyle ??
-      originalGetComputedStyle(element)
-    );
-  };
+    return new Proxy(emptyStyle, {
+      get(_, name) {
+        const key = name as keyof CSSStyleDeclaration;
 
-  return () => {
-    window.getComputedStyle = originalGetComputedStyle;
+        const mockedStyle =
+          elementToStyle.get(element) ?? defaultStyle ?? emptyStyle;
+
+        const actualStyle = originalGetComputedStyle(element);
+
+        return name in mockedStyle ? mockedStyle[key] : actualStyle[key];
+      }
+    });
   };
 }
 
@@ -33,4 +38,8 @@ export function setElementStyle(element: Element, style: CSSStyleDeclaration) {
   elementToStyle.set(element, style);
 
   emitter.emit("change", element);
+}
+
+export function unmockGetComputedStyle() {
+  window.getComputedStyle = originalGetComputedStyle;
 }
