ab61ee9688fc491d9108eb5b5e02c90a8e2dd8ea
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-04-12 14:46:59 -0400

    Upgrade react-lib-tools to improve site-search

--- scripts/compile-search-index.ts
+++ scripts/compile-search-index.ts
@@ -1,4 +1,4 @@
-import { compileSearchIndex } from "react-lib-tools/scripts/compile-search-index.ts";
+import { compileSearchIndex } from "../../react-lib-tools/scripts/compile-search-index.ts";
 
 await compileSearchIndex({
   chromeExecutablePath: process.env.CHROME_PATH,
