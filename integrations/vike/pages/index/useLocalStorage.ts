335d7f202674723640b97ff1bf167cc42f36e459
Brian Vaughn <brian.david.vaughn@gmail.com>
2026-05-29 16:42:33 -0400

    Remove temp did-mount state from e2e test harness

--- integrations/vike/pages/index/useLocalStorage.ts
+++ integrations/vike/pages/index/useLocalStorage.ts
@@ -1,27 +1,21 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import type { LayoutStorage } from "react-resizable-panels";
 
 export function useLocalStorage(): LayoutStorage {
-  const [didMount, setDidMount] = useState(false);
-
-  useEffect(() => {
-    setDidMount(true);
-  }, []);
-
   return useMemo(
     () => ({
       getItem: (key: string) => {
-        if (didMount && typeof window !== "undefined") {
+        if (typeof window !== "undefined") {
           return localStorage.getItem(key);
         }
         return null;
       },
       setItem: (key: string, value: string) => {
-        if (didMount && typeof window !== "undefined") {
+        if (typeof window !== "undefined") {
           localStorage.setItem(key, value);
         }
       }
     }),
-    [didMount]
+    []
   );
 }
